7ec5fe2c603987c5a8d48ee44a1e992496007e73
mdsiamss10 <mdsiamss10@gmail.com>
2023-06-09 12:49:51 +0600

    direct signin pagw without login screen of next auth

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -4,7 +4,10 @@ import { signIn } from "next-auth/react";
 
 function LoginButton() {
   return (
-    <button className="btn btn-accent text-white" onClick={() => void signIn()}>
+    <button
+      className="btn btn-accent text-white"
+      onClick={() => void signIn("auth0")}
+    >
       LOGIN
     </button>
   );
